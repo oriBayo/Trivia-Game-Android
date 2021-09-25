c94bcfedaca1da4294f9e6ff2af9fb9904dafe12
oriBayo <64733813+oriBayo@users.noreply.github.com>
2021-09-25 15:18:46 +0300

    Authentication and activites
    1) create 5 activities:
    start ,login, register, menu, game.
    2) Authentication with email & password.

---
+++ TriviaGame/app/src/main/java/com/example/triviagame/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.triviagame;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email_input);
+        password = findViewById(R.id.password_input);
+        login = findViewById(R.id.login_btn);
+
+        auth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                loginUser(txt_email,txt_password);
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
